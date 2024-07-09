d16e3130669ab59db6bd7a088a0249a96f5bc6fd
mardon1dev <dusbekovmardonbek@gmail.com>
2024-07-09 14:09:05 +0500

    Update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 let button = document.querySelector(".header__burger");
 let menu = document.querySelector(".header__menu");
 let body = document.querySelector("body");
-let header = document.querySelector(".header");
+let header = document.querySelector("header");
 button.addEventListener("click", function () {
     button.classList.toggle("toggle");
     menu.classList.toggle("active");
     body.classList.toggle("scroll");
 });
-
+window.addEventListener("scroll", function(){
+    if(window.pageYOffset > 0){
+        header.classList.add("header-active");
+    }
+    else{
+        header.classList.remove("header-active");
+    }
+})
 
